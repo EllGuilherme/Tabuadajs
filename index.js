66b84bc8f49dc6adfd3c6b2ed9f3031d5a8499fc
EllGuilherme <grds1000@gmail.com>
2022-11-07 22:10:35 -0300

    final da tarefa de tabuada

---
+++ index.js
@@ -0,0 +1,14 @@
+const numerodesejado=prompt("Tabuada do número:")
+let numero = 0
+let contador = 1
+let resultado = 'Tabuada do Número'
+
+while (numero < 20)
+
+{
+numero = numero + 1
+contador = numerodesejado * numero
+resultado = `${resultado} \n${numerodesejado} x ${numero} = ${contador}` 
+}
+alert(resultado);
+
